0a40f20d12f9ec0d2d542bfcfd95d35e08afe215
NikPurvis <nikpurvis@gmail.com>
2022-02-17 06:53:46 -0700

    Reddit fetch deliverable hw#7 (incomplete) -- Nikki
    Sleep hit hard yesterday so this is only just started, but will continue to work until complete!

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Requirements/User Experience
+// Page should load with
+// Some sort of title
+// A short description telling the user what to do
+// A blank text field
+// A Button ("start" or "go" or "search")
+// When the user enters a search term and presses enter
+// The form / title / description should hide
+// Show a loading message (optional)
+// Fetch related posts from reddit (with fetch)
+// Display animation / slideshow of images (with DOM manipulation)
+// Show a button to stop slideshow
+// Repeat animation until user clicks "stop", then redisplay the original form/title/description
+// When the user clicks the "stop" button
+// Animation stops / images are removed
+// Form / title / description are shown again
+// User can enter a new search term
+
+console.log("Hi, I'm here!")
+
