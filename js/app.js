a5270ac7a04c7329b3c47113cad5466758b6cab2
cloud3999 <edwardlcloud@gmail.com>
2022-02-18 00:42:51 -0500

    created a loop to interpet img links
    The loop pushes each link into an array. Attempted to create a loop that writes each array item into an HTML element. This is currently non functional.

--- js/app.js
+++ js/app.js
@@ -3,24 +3,32 @@ document.addEventListener("DOMContentLoaded", () => {
     const searchInput = document.querySelector('#searchInput')
     const submitButton = document.querySelector('#submitButton')
     const stopButton = document.querySelector('#stopButton')
+    const imgBox = document.getElementById('imgBox')
+
+    
+
   
   //show and hide HTML Elements
-  const hideElement = () => {
-    let x = document.getElementById('submitForm')
-    if (x.style.display === "none") {
-        x.style.display = "inline";
-    } else {
-        x.style.display = "none" ;
-    }
+    const hideElement = () => {
+        let x = document.getElementById('submitForm')
+        if (x.style.display === "none") {
+            x.style.display = "inline";
+        } else {
+            x.style.display = "none" ;
+        }
 
-    let y = document.getElementById('stopButton')
-    if (y.style.display === "none") {
-        y.style.display = "inline";
-    } else {
-        y.style.display = "none" ;
-    }
+        let y = document.getElementById('stopButton')
+        if (y.style.display === "none") {
+            y.style.display = "inline";
+        } else {
+            y.style.display = "none" ;
+        }
 
-
+        if (imgBox.style.display === 'none') {
+            imgBox.style.display = 'inline';
+        } else {
+            imgBox.style.display = 'none' ;
+        }
     }
 
   
@@ -37,12 +45,32 @@ document.addEventListener("DOMContentLoaded", () => {
             })
             // locate our data and store it so we can render
             .then(jsonData => {
-                console.log(jsonData)
-                console.log('this is the data we want:', jsonData.data.children[1].data.url)
-                // let people = jsonData.results
-                // use that data to create some HTML elements to display
-                // render those elements
-                // people.forEach(addPerson)
+                console.log('here is jsonData' , jsonData)
+                
+                //array to store the images called
+                let imagesArr = []
+
+                for (let i = 0; i <= 15; i++) {
+                 
+                    if (jsonData.data.children[i].data.url.includes('jpg' || 'png')) {
+                    console.log(jsonData.data.children[i].data.url) 
+                    imagesArr.push(`${jsonData.data.children[i].data.url}`)
+                    }
+                    console.log('these are the images', imagesArr)              
+                }
+                if (imagesArr === [""]) {
+                    alert("No image URLs found. Please try a different query")
+                }
+                    
+                for (let i = 0; i < 6; i++ ) {
+                    let imgDestination = document.getElementsByClassName('carousel-inner')
+                    let createImgHTML = document.createElement('div')
+                    createImgHTML.className = ('carousel-item')
+                    createImgHTML.innerHTML = (`<img src="${imagesArr[i]}" class="d-block w-100" data-bs-interval="3000"`)
+                
+                }
+            
+                
             })
 
         .catch(err => console.log('error!', err))
@@ -55,6 +83,9 @@ document.addEventListener("DOMContentLoaded", () => {
       event.preventDefault()
       // console.log('clicky', event)
       hideElement()
+      imagesArr = []
+      console.log('is imagesArr empty?', imagesArr)
+    
 
     })
 
