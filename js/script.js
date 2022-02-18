d467c5d48cf7237c4f8238c75708b6682ee46da2
Ivycle94 <Ivycle94@gmail.com>
2022-02-18 01:42:27 -0600

    commit_1-fetch_reddit_slideshow

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// console.log("hi")
+
+// Make a request for reddit API
+document.addEventListener("DOMContentLoaded", () => {
+    const requestURL = "http://www.reddit.com/search.json?q=cats+nsfw:no"
+    const textInput = document.querySelector("#textInput")
+    const searchButton = document.querySelector("#searchButton")
+
+})
+
+
+
+// make searcg button work
+searchButton.addEventListener("click", (e) => {
+    fetch(`re`)
+})
