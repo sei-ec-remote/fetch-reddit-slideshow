cdf798a215c3ca3b826c4c3bc70e72140253e798
Ivycle94 <Ivycle94@gmail.com>
2022-03-23 20:09:26 -0500

    completed deliverable

--- js/script.js
+++ js/script.js
@@ -1,16 +1,58 @@
 // console.log("hi")
-
-// Make a request for reddit API
 document.addEventListener("DOMContentLoaded", () => {
-    const requestURL = "http://www.reddit.com/search.json?q=cats+nsfw:no"
-    const textInput = document.querySelector("#textInput")
-    const searchButton = document.querySelector("#searchButton")
 
+const searchButton = document.querySelector("#searchButton")
+const stopButton = document.querySelector("#stopButton")
+const title = document.querySelector("#title")
+const requestURL = "http://www.reddit.com/search.json?q="
+const textInput = document.querySelector("#textInput")
+const info = document.querySelector("#info")
+const imgUrl = document.getElementById("img")
+let imageArray = []
+let counter = 0
+
+
+stopButton.addEventListener("click", (e) => {
+    // title.style.display = ''
+    // info.style.display = ''
+    // searchButton.style.display = ''
+    // textInput.style.display = ''
+    clearInterval(slide)
 })
 
-
-
-// make searcg button work
 searchButton.addEventListener("click", (e) => {
-    fetch(`re`)
+    e.preventDefault()
+    searchButton.remove()
+    textInput.remove()
+    info.remove()
+    title.remove()
+    fetch(`${requestURL}${textInput.value}+nsfw:no`)
+    .then(apiResponse => {
+        return apiResponse.json()
+    })
+    .then(jsonData => {
+        console.log(jsonData)
+        let images = jsonData.data.children
+        let thumb = 0
+        console.log("this is images", images)
+        for (let i = 0;i < images.length; i++) {
+            thumb = images[i].data.thumbnail
+            if (thumb.includes(".png")||thumb.includes(".jpg")) {
+                console.log("this is thumb",thumb)
+                imageArray.push(thumb)
+            }
+            console.log("this is imageArray", imageArray)
+        }
+        const cycle = () => {
+            imgUrl.src = imageArray[counter]
+            while (counter === imageArray.length - 1) {
+                counter =- 1
+            }
+            counter ++
+        }   
+        let slide = setInterval(cycle, 500)
+        console.log(counter)
+    })
+    .catch(err => console.log('error!', err))
+})
 })
