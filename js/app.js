0150bbba5771b408c9861bdf67f538dc5155f564
Nash4star <forrester.nash@gmail.com>
2022-04-09 19:40:10 -0400

    restarted code to try and get it to work

--- js/app.js
+++ js/app.js
@@ -1,66 +1,36 @@
-const API_URL = 'http://www.reddit.com/search.json?q=cats+nsfw:no'
-const INTERVAL_DELAY = 2000
-let currentImages = []
-let currentIndex = 0
-let interval = null
+document.addEventListener('DOMContentLoaded', () => {
 
-document.getElementById('search-form').addEventListener('submit', e => {
+const text = document.getElementById('text')
+const reqUrl = `http://www.reddit.com/search.json?q=${text.value}+nsfw:no`
+const stopB = document.getElementById('stop')
+const searchB = document.getElementById('search')
+const pic = document.getElementById('pic')
 
+
+searchB.addEventListener('click', (e) => {
     e.preventDefault()
 
-    let userQuery = document.getElementById('query').value
-
-        if (userQuery) {
-            fetchReddit(userQuery)
-            document.getElementById('query').vaule = ''
-        }
-})
-const fetchReddit = (query) => {
-    fetch(API_URL + query)
-    .then(response => response.json())
-    .then(jsonData => {
-        currentImages = jsonData.data.children.map(p => {
-            return {
-                title: p.data.title,
-                url: p.data.url
+    fetch(`${reqUrl}`)
+        .then((res) => {
+            return res.json()
+        })
+        .then((json) => {
+            // console.log(json.data.children[0].data.url)
+            let cats = json.data.children
+            let show = 0
+            let imagecontainer = document.createElement('div')
+            for (let i = 0; i < cats.length; i++) {
+                show = cats[i].data.url
+                if (show.includes('.jpg')) {
+                    const img = document.createElement('img')
+                    
+                    imagecontainer.appendChild(img)
+                    img.src = show
+                    
+                }
+                console.log(show)
             }
         })
-        startSlideshow()
-    })
-    .catch(err => {
-        console.log('ERROR', err)
-    })
-}
-const startSlideshow = () => {
-    currentIndex = 0
-
-    placeImage()
-    document.getElementById('form-container').style.display = 'none'
-    document.getElementById('slideshow-container').style.visibility = 'visible'
-    
-    interval = setInterval(changeImage, INTERVAL_DELAY)
-}
-
-const changeImage = () => {
-    currentIndex++
-    if (currentIndex >= currentImages.length) {
-        currentIndex = 0
-    }
-    placeImage()
-}
-
-const placeImage = () => {
-    document.getElementById('result').innerHTML = ''
-    let img = document.createElement('img')
-    img.src = currentImages[currentIndex].url
-    img.alt = currentImages[currentIndex].url
-
-    document.getElementById('result').append(img)
-}
-
-document.getElementById('stop-button').addEventListener('click', () => {
-    document.getElementById('slideshow-container').style.visibility = 'hidden'
-    document.getElementById('form-container').style.display = 'block'
-
-    clearInterval(interval)
 })
+})
+
