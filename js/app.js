fd76b4f61a18e8811c5272da81e536d806833f48
“aquador” <Allison.quador@gmail.com>
2022-04-10 19:43:33 -0600

    logging data now

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 // We want to make a request to the Random User API
 document.addEventListener("DOMContentLoaded", () => {
     //request URL base, the query input is populated by the user
-    const requestURL = "http://www.reddit.com/search.json?q="
+    const requestUrl = "http://www.reddit.com/search.json?q="
+    //search input value entered by the user
+    const searchInputValue = document.querySelector('#search')
     const image = document.getElementById('#image')
-    const peopleList = document.querySelector('#peopleList')
     const submitButton = document.querySelector('#submitButton')
     const stopButton = document.querySelector('#stopButton')
-
+    // Create an array of image URLs (tip: use filter and map)
+    let images = []
+    //Create button to stop animation (tip: use clearInterval)
+    stopButton.addEventListener('click', (e) => {
+        clearInterval()
+    })
     // click listener to submit request on click
     submitButton.addEventListener('click', (event) => {
         // stops page from reloading on form submit
         event.preventDefault()
         // console.log('clicky!', event)
         // extract the value from our number input and send it our request
-        fetch('https://www.reddit.com/search.json?q=')
+        fetch(`${requestUrl}${searchInputValue.value}`)
         // handle the response that we receive (convert it to JSON)
         .then(apiResponse => {
             return apiResponse.json()
@@ -21,11 +27,13 @@ document.addEventListener("DOMContentLoaded", () => {
         // locate our data and store it so we can render
         .then(jsonData => {
             console.log(jsonData)
-            console.log('this is the data we want:', jsonData.results)
-            let people = jsonData.results
-            // use that data to create some HTML elements to display
-            // render those elements
-            people.forEach(addPerson)
+            let searchImages = jsonData.data.children
+            console.log('this is the data returned from search:', searchImages)
+            
+           
+            // Cycle through images
+            // tip: use setInterval
+            // Either add images, or change the src of a single image tag
         })
             .catch(err => console.log('error!', err))
         // console.log('number input value:', numberInput.value)
