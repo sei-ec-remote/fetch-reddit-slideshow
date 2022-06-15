a6101e12bd667ca1a08f35f977ecf2f335748dde
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-06-15 15:35:27 -0400

    Foundation made && the  replaning

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// User Map
+// Type a search into fuild#input and hit #submit
+// computer fetches it with the #input field
+// updates the container with pictures
+// User hit a back button to start over.
+
+//js ingredients
+    //Message strArr for the submit button, when there no .backButton on it
+    //The button #submit //To request the search
+    //the input #input //To get user search request
+    // div #container to hold the users answers
+
+// funct start search //arg searchItem
+    //if !.backButton
+        // fetches 
+        // fill the container with <img>
+        // toggle my submit/backbutton add class name to .backButton
+    //else
+        // remove content of div
+        // toggle my submit/backbutton remove class name to .backButton 
+
+// funct toggle my submit/back button
+    //if .backButton 
+        //remove .backButton
+        //change innerText = message[0]
+    //else 
+        //add .backButton
+        //change innerText = message[1]
+
+//addEventL
+    // on #submit to start the search
+        //pass input value #input
