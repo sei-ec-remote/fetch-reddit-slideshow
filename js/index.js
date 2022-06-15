ef275bab2ffddef75002438ef6c24b57b9f159f7
nickesparza <nickesparza@gmail.com>
2022-06-15 18:51:57 -0500

    add functionality to show image array, go back to search window, and enter/display a new set of images

--- js/index.js
+++ js/index.js
@@ -1,53 +1,78 @@
+// capture container with title and search bar
+const startContainer = document.querySelector('#start-container')
 
 // capture container for slides
 const slideContainer = document.querySelector('#slide-container')
+
+// capture stop button
+const stopButton = document.querySelector('#stop-button')
 
 // capture form element
 const form = document.querySelector('#form')
 
 // initialize an empty array for storing image URLs
-const gallery = []
+// const gallery = []
+
+// function to initiate gallery view
+const showGallery = (array) => {
+    array.forEach(picture => {
+        // // check if a slide with class image-slide exists already
+
+        // create a new empty div
+        const imgDiv = document.createElement('div')
+
+        // add image-slide class
+        imgDiv.classList.add('image-slide')
+
+        // set innerHTML of the div to the src URL of the array element
+        imgDiv.innerHTML = `<img src="${picture}">`
+        
+        // console.log(imgDiv.innerHTML)
+        // append div with image to container
+        slideContainer.appendChild(imgDiv)
+
+        // set timeout before moving to next photo
+    })
+}
 
 // handle reddit fetch success
 const onRedditFetchSuccess = (redArray) => {
+    // set display of start-container to none
+    startContainer.style.display = 'none'
+    // set display of slide container and stop button to visible
+    slideContainer.style.display = 'block'
+    stopButton.style.display = 'block'
     // console log the top level JSON object
     console.log(redArray)
+    
     // set variable for the array of results
     const redditResult = redArray.data.children
+    
     // log array of results
     console.log(redditResult)
+
+    // initialize an empty array for storing image URLs
+    const gallery = []
+
     // iterate over array of results to check the preview image property
     redditResult.forEach(element => {
         // if the array element has a preview property, create a div and push it to the page
         if (element.data.hasOwnProperty('preview') && element.data.preview.images[0].resolutions[3]) {
+            
             // set variable for the URL of the preview image
             const imgURL = element.data.preview.images[0].resolutions[3].url
+
+            // push the url into the gallery array
             gallery.push(imgURL)
-            // create div to be pushed
-            const imgDiv = document.createElement('div')
-            imgDiv.classList.add('image-slide')
-            // set innerHTML of the div to the src URL of the array element
-            imgDiv.innerHTML = `<img src="${imgURL}">`
-            // console.log(imgDiv.innerHTML)
-            // append div with image to container
-            document.querySelector('#slide-container').appendChild(imgDiv)
         }
     })
     console.log(gallery)
+    showGallery(gallery)
 }
 
 const onRedditFetchFailure = () => {
     console.log('reddit fetch failed')
 }
-
-// initialize fetch from reddit on clicking a button
-// document.addEventListener('click', (event) => {
-//     event.preventDefault()
-//     fetch(`https://www.reddit.com/r/pics/search.json?q=cats&limit=50&restrict_sr=1&sr_nsfw=`)
-//         .then(response => response.json())
-//         .then(onRedditFetchSuccess)
-//         .catch(onRedditFetchFailure)
-// })
 
 form.addEventListener('submit', (event) => {
     // prevent the default behavior of 'refresh page'
@@ -56,10 +81,24 @@ form.addEventListener('submit', (event) => {
     // capture user input
     const userSearch = input.value
 
-    fetch(`https://www.reddit.com/r/pics/search.json?q=${userSearch}&limit=50&restrict_sr=1&sr_nsfw=`)
+    fetch(`https://www.reddit.com/r/pics/search.json?q=${userSearch}&limit=5&restrict_sr=1&sr_nsfw=`)
         .then(response => response.json())
         .then(onRedditFetchSuccess)
         .catch(onRedditFetchFailure)
+})
+
+stopButton.addEventListener('click', () => {
+    // remove children of slide container
+    while (slideContainer.firstChild) {
+        slideContainer.removeChild(slideContainer.firstChild)
+    }
+
+    // set slide container and stop button to display: none
+    slideContainer.style.display = 'none'
+    stopButton.style.display = 'none'
+
+    // re-display the start container
+    startContainer.style.display = 'block'
 })
 
 // initialize fetch from reddit on DOM Content Loaded
