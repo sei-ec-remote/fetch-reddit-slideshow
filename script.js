09041006aadc2f72652ed8b8a479fdab836e152f
jamestylladams <jamestylladams@gmail.com>
2022-06-17 15:20:38 -0600

    Finish slideshow

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
 const search = document.getElementById("search")
 const imageDisplay = document.getElementById("scuz")
+const greeeting = document.getElementById("greeting")
+const stop = document.getElementById('stop')
 let goatArray = []
 let imageIndex = 0
-//let url
 
 const changeImage = () => {
     imageDisplay.setAttribute('src', goatArray[imageIndex].data.url)
     imageIndex = (imageIndex + 1) % goatArray.length
-    setTimeout(changeImage, 2000);
+    setTimeout(changeImage, 2000)
 
 }
 
 const onSucces = (stuff) => {
-    
     children = stuff.data.children
     
     const imageArr = children.filter((datNew) => {
         if(datNew.data.url.includes("imgur")){
             return false
-        }else if(datNew.data.url.includes("jpg")){
-            return datNew
-        }else if(datNew.data.url.includes("png")){
-            return datNew
-        }else if(datNew.data.url.includes("jpeg")){
-            return datNew
+        }else{
+            return true
         }
     })
-    console.log(`${url}`)
     goatArray = imageArr
-    console.log(imageArr)
+    console.log(goatArray)
     changeImage()
+    searchHide()
 }
 
 const onFailure = () => {
     console.log('failure')
 }
 
+const searchHide = () => {
+    search.style.display = "none"
+    greeeting.style.display = "none"
+    stopIt.style.display = "flex"
+}
+
+noMoreShow = () => {
+    clearTimeout(changeImage)
+    imageDisplay.removeAttribute('src')
+    search.style.display = "flex"
+    greeeting.style.display = "flex"
+    stopIt.style.display = "none"
+    goatArray = []
+}
+
+stopIt.addEventListener("click", noMoreShow)
+
 search.addEventListener('keypress', e => {
     
     if (e.key === 'Enter'){
-        url = `http://www.reddit.com/r/pics/search.json?q=${search.value}+nsfw:no`
+        url = `http://www.reddit.com/search.json?q=${search.value}+nsfw:no+type:image`
         fetch(url) 
        .then(rep => rep.json())
        .then(onSucces)
        .catch(onFailure)
-
     }
 });
 
