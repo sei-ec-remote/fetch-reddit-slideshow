122eb2efedf3d826b029f1b9735a628ecfc4819b
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-06-18 12:48:49 -0400

    Pictures load what was searched for.

--- js/index.js
+++ js/index.js
@@ -2,13 +2,9 @@ const p = (str) => {console.log(str)}
 //js ingredients
     //Message strArr for the submit button, when there no .backButton on it
     // messageHolder = 
-    const form = document.querySelector('#form')
+    const form = document.querySelector('#actionButton')
 
     //The button #submit //To request the search Or go back
-    //const submit = document.getElementById('submit')
-    //the input #input //To get user search request
-    //input = document.getElementById('#input')
-    // div #container to hold the users answers
     resaltsPanal = document.getElementById('container')
 
 // funct toggle my submit/back button
@@ -27,50 +23,32 @@ const toggleSubmitButton = () => {
     //     submit.innerText = buttonMessage[0]
     // }
 }
+const slideShow = () => {
+    let slideindex = 0 
+}
 const onPicterFailure = () => {console.log('This has FAILED')}
 /////////////////Main Function
 const onPictureSeccess = (picture) => {
-    p(picture.data.children[0].data.url)
-    p(picture.data.children[3].data.url)
-    p(picture.data.children[2].data.url)
-    p(picture.data.children[21].data.url)
-    p(picture.data.children[23].data.url)
-    p(picture.data.children[24].data.url)
-    p(picture.data.children[19].data.url)
     
-    p(picture.data.children.length)
-    
+    //Clear out the old img if there is any.
+    const oldSlideDeck = document.querySelectorAll('.slide-deck')
+    if (oldSlideDeck) {
+        oldSlideDeck.forEach(img => {
+            img.remove()
+        })
+    }
     const pictureURL = []
-    //picture.data.children[23].data.url
     for (let index = 0; index < picture.data.children.length; index++) {
-        // p('inside the first belly')
-        // p(`picture.data.children[${index}].url`)
-        // p(picture.data.children[index].data.url)
-        // p(picture.data.children[0].data.url)
-
-        pictureURL[index] = picture.data.children[index].data.url
-        
+        pictureURL[index] = picture.data.children[index].data.url   
     }
-    // p(pictureURL)
-    // if (picture.data.children[23].data.preview.images[0].source[0].url == pictureURL) 
-        //p('true')
-//    picuter = 
-        // p("did I get passed the first if?")
     for (let i = 0; i < pictureURL.length; i++) {
-        // p("Did I make it in the second for")
         const img = document.createElement('img')
+        img.classList.add('slide-deck')
+        // img.style.display = 'none'
         img.setAttribute('src', pictureURL[i])
-        resaltsPanal.appendChild(img)
-        
+        resaltsPanal.appendChild(img) 
     }
-    // p("did I get passed the second if?")
-    
-    // 
-    // preview
-    // "images": [
-    //     {
-    //         "source": {
-    //           "url":
+    // slideShow() //slideshow alll <img> in <div.container>
     //if !.backButton
         // fetches 
         // fill the container with <img>
@@ -92,8 +70,8 @@ const onPictureSeccess = (picture) => {
 // // on #submit to start the search
 form.addEventListener('click',  (e) => {
     e.preventDefault()
-    const picture = input.value
-
+    let picture = input.value
+ 
     fetch(`https://www.reddit.com/search.json?q=${picture}+nsfw:no+type:image`)
         .then(res => res.json())
         .then(onPictureSeccess)
