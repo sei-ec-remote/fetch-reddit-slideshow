5a567329c03b8d3ca40a86b7a5fba7cf0c3440d4
RandyD <rdsoundguy@gmai.com>
2022-06-20 11:38:42 -0400

    Needs to conform to requirements and URL cleaning

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// TO DO:
+// conform to requirements below
+// Clean URL list
+
+
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 let globalImgUrlsArray = [] // global USED VAR TO keep it globally scoped
 
 const form = document.querySelector('#form')  //global
@@ -103,3 +110,31 @@ form.addEventListener('submit', (event) => {
            console.log("Oh no, there's been an error!", error)})
 
 })
+
+
+//////////**************************************************************
+
+
+// Requirements/User Experience
+// Page should load with
+
+//     Some sort of title  DONE
+//     A short description telling the user what to do DONE
+//     A blank text field DONE
+//     A Button ("start" or "go" or "search") DONE
+
+// When the user enters a search term and presses enter
+
+//     The form / title / description should hide !!! 
+//     Show a loading message (optional) 
+//     Fetch related posts from reddit (with fetch) DONE
+//     Display animation / slideshow of images (with DOM manipulation) DONE
+//     Show a button to stop slideshow !!!
+//     Repeat animation until user clicks "stop", then redisplay the original form/title/description !!!
+
+// When the user clicks the "stop" button
+
+//     Animation stops / images are removed !!!
+//     Form / title / description are shown again
+//     User can enter a new search term
+// */
