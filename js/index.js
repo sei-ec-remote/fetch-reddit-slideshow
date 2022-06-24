458cbc465e68252b932da82b8a83889eec0ace62
Zene09 <Zeneorr@gmail.com>
2022-06-24 12:00:36 -0500

    Completed Fetch Reddit

--- js/index.js
+++ js/index.js
@@ -4,15 +4,33 @@
     const seeResults = document.querySelector('#input')
     // get search form
     const form = document.querySelector('#form')
+    // assign carousel
+    const carousel = document.querySelector('#carousel')
 
+    const arrayurl = []
     
-    const onGetResultSuccess = () => {
-        console.log('get result success')
-    }
-    const onGetResultFailure = () => {
-        console.log('failed to get results')
-    }
-    const onShowResultSuccess = () => {
+    // const onGetResultSuccess = () => {
+    //     console.log('get result success')
+    // }
+    // const onGetResultFailure = () => {
+    //     console.log('failed to get results')
+    // }
+    const onShowResultSuccess = (json) => {
+        // console.log(json.data.children)
+        for (let i = 0; i < 6; i++) {
+            arrayurl[i] = json.data.children[i].data.url
+        }
+        let i = 1
+        // console.log(arrayurl)
+        arrayurl.forEach(image => {
+            console.log(image)
+            document.getElementById(`slide${i}`).setAttribute('src', `${image}`)
+            i++
+            container.style.display = 'none'
+            carousel.style.display = 'flex'
+        })
+
+        // console.log(json)
         console.log('show result success')
     }
     const onShowResultFailure = () => {
@@ -29,27 +47,25 @@
             
         })
         // once the DOM is loaded run said block of code
-        document.addEventListener('DOMContentLoaded', () => {
-            // fetch is an interface of AJAX
-            fetch(`https://www.reddit.com/search.json?`)
-            .then(res => (res.json()))
-            .then(res => res.data.children)
-            .then(res => res.map(post => ({
-                author: post.data.author,
-                link: post.data.url,
-                img: post.data.thumbnail,
-                title: post.data.title
-            })))
-            .then(onGetResultSuccess)
-            .catch(onGetResultFailure)
-    })
+    //     document.addEventListener('DOMContentLoaded', () => {
+    //         // fetch is an interface of AJAX
+    //         fetch(`https://www.reddit.com/search.json?`)
+    //         .then(res => (res.json()))
+    //         .then(res => res.data.children)
+    //         .then(res => res.map(post => ({
+    //             author: post.data.author,
+    //             link: post.data.url,
+    //             img: post.data.thumbnail,
+    //             title: post.data.title
+    //         })))
+    //         .then(onGetResultSuccess)
+    //         .catch(onGetResultFailure)
+    // })
 
-let index = 0
-let img = document.querySelector('img')
-let results 
 
-function render(res) {
-    results = res
-    img.setAttribute("src", res[index].link)
-    index = index < results.length == index + 1;
-}
+const reset = document.getElementById('stop-slide')
+reset.addEventListener('click', function(){
+    container.style.display = 'flex'
+    carousel.style.display = 'none'
+    
+})
