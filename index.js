ef8e1e36b4b173df75e61b364354e278af448032
SunGod7 <sun7ruler@gmail.com>
2022-06-28 08:19:57 -0400

    struggled with this

--- index.js
+++ index.js
@@ -1,90 +1,81 @@
 
+// document.querySelector('#stopBtn').addEventListener('click ', (Event) =>{
+//      clearInterval(timeSet)
+//    })
 
+//const stopBtn = document.getElementById("stopBtn");
+//stopBtn.addEventListener("click", mainloop);
+// const container = document.querySelector('#container')
+// const seeAllPics = document.querySelector('#see-all-pics')
+// const form = document.querySelector('#form')
+//container.style.display = 'none'
 
-// //const container = document.querySelector('#container')
-// //const form = document.querySelector('#form')
-// //const search = document.querySelector('#search-btn')
-//  //('https://i.redd.it/h72g3ogizs691.jpg', reddits.url)
-   
-//  const onGetRedditCatSuccess = (redditCatArray) => {
-//       // console.log('redditCatArray')
-
-//  }
 let i = 0
-  const mainloop = (redditobject) => {
-    
-    
-    let arrayOfUrls =[] 
+  
+
+//let start = false
+
+
+const mainloop = (redditobject) => {
+  
+  
+     
+  let arrayOfUrls =[] 
     
     //console.log(redditobject.data.children[0].data.url)
     
     for(let i = 0; i < 25; i++ ){
-      arrayOfUrls[i] =  redditobject.data.children[i].data.url                      // redditobject.data.children[i].data.url)
-       
-    }
-    const innerLoop =( ) => {
+      arrayOfUrls[i] =  redditobject.data.children[i].data.url 
+      
+        const innerLoop =( ) => {
    
-    if( i > 24){
-      location.reload()
-    }
+            if( i > 24){
+             location.reload()
+        }
 
     
-    //getImg.style.height(300)
-  const getImg = document.querySelector('#catImg')
-  catImg.src = arrayOfUrls[i]
-  // console.log(arrayOfUrls[0])
+     document.getElementById("catImg").style.height = "600px";
+     const getImg = document.querySelector('#catImg');
+     catImg.src = arrayOfUrls[i];
+     console.log(arrayOfUrls[0]);
 
-  i++
-}
+        i++
+    }
+  //let timeSet = setInterval(innerLoop,3000)
   
-
-//let timeSet = setInterval(innerLoop,3000)
   
-}
-
-
-
-
- 
-// const stopSlide = () => {
-
-// }
-
-// document.querySelector('#stopButton').addEventListener('click ', (Event) =>{
-//   clearInterval(timeSet)
-// })
-// const getImg1 = document.querySelector('#catImg')
-// catImg.src = arrayOfUrls[1]
-// console.log(arrayOfUrls[1])
-
-// for(let i = 0; i < 25; i++ ){
-//   arrayOfUrls[i] =  redditobject.data.children[i].data.url                      // redditobject.data.children[i].data.url)
    
-// }
-
-
-// const getImg2 = document.querySelector('#catImg')
-// catImg.src = arrayOfUrls[2]
-// console.log(arrayOfUrls[2])
-
-// for(let i = 0; i < 25; i++ ){
-//   arrayOfUrls[i] =  redditobject.data.children[i].data.url                      // redditobject.data.children[i].data.url)
-   
-// }
-
-
-// const getImg3 = document.querySelector('#catImg')
-// catImg.src = arrayOfUrls[3]
-// console.log(arrayOfUrls[3])
-
-// setInterval(function() {
-                        // redditobject.data.children[i].data.url)
-     
-//   }
-
-// }5000)
   
-//   }
+
+  }
+  
+}
+
+
+// function startGame() {
+//   let startDiv = document.getElementById("start");
+//   let gameOver = document.getElementById("game-over");
+//   startDiv.style.display = "none";
+//   gameOver.style.display = "none";
+//   //startGame();
+// }
+
+// function gameOver() {
+//   let startDiv = document.getElementById("start");
+//   let gameOver = document.getElementById("game-over");
+//   startDiv.style.display = "none";
+//   gameOver.style.display = "block";
+//   let i = 0;
+//   //gameOver();
+// }
+
+
+
+
+
+
+
+
 
  document.addEventListener('DOMContentLoaded', () =>  { 
 
@@ -104,38 +95,26 @@ let i = 0
 })
 
 
-// const memeBtn = document.getElementById('memeBtn')
-//  const after=''
 
-//  function fetchMemes() {
-//    //changeColor
 
   
+ 
 
 
 
-//    let parentdiv = document.createElement('div')
-//         parentdiv.id = 'memes'
-//         fetch(`http://www.reddit.com/r/memes.json`)
-//         .then(response => response.json())
-//           .then(body => {
-//            for (let index = 0; index < body.data.childrenlength; index++){
-//             if (body.data.children[index].data.post_hint ==='image'){
-//                let div = document.createElement('div')
-//                let h4 = document.createElement('h4')
-   //             let image = document.createElement('img')
-   //             image.src = body.data.children[index].data.url_overridden_by_dest
-   //             h4.textContent = body.data.children[index].data.title
-   //             div.appendChild(h4)
-   //             div.appendChild(image)
-   //             parentdiv.appendChild(div)
-              
 
-   //          }
-   //         }
-   //         document.body.appendChild(parentdiv)
-           
-   //     })
 
-   // }
-  
+//document.getElementById("container").style.border = "thick solid #0000FF";
+ //document.getElementById('form').addEventListener('sumbit', function(){})
+
+
+ 
+const stopSlide = () => {
+
+  mainloop()
+
+}
+
+document.querySelector('#startBtn').addEventListener('click ', (stopSlide) =>{
+  clearInterval(timeSet)
+ })
