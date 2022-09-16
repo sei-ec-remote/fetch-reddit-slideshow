08077353d9d8c8e2ea43b40c8f1370d4b802bb54
skbarbee <barbee.s.k@gmail.com>
2022-09-15 23:54:40 -0400

    need to remove header and stop slideshow

--- js/index.js
+++ js/index.js
@@ -3,15 +3,60 @@ const stopButton = document.querySelector ('#stop')
 const  beginningInfo = document.querySelector("#header")
 const userInput = document.querySelector("#user-input")
 const form = document.querySelector('#userRequest')
+const pictureFrame = document.querySelector("#slides-container")
+// let slides = document.querySelectorAll(".mySlides")
 
-form.addEventListener('click', event => {
-    // prevent default behavior of refresh
-    // almost will always have to do this
-    event.preventDefault()
-})
 
-searchButton.addEventListener('click', ()=>{
+const initalArray = ()=>{
+    
+    fetch(`http://www.reddit.com/search.json?q=${userInput.value}+nsfw:no`)
+    .then(res => res.json())
+    .then(makeImageArray)
+    // .then(res => console.log(res.thumbnail))
+    .catch(console.error)
+}
+const makeImageArray = (jsonArray) =>{
    
-    console.log(userInput.value)
+    const imageArray = jsonArray.data.children.map((element) =>{
+        return element.data.thumbnail  
+    })
+    imageArray.forEach((url)=>{
+        
+        // pictureDiv.setAttribute("class", "mySlides")
+        // pictureDiv.setAttribute("id", "fade")
+        
+       const picture = document.createElement('img')
+        picture.setAttribute("class","imageSlides")
+        picture.setAttribute("src", url)
+        pictureFrame.appendChild(picture)
+       //console.log(pictureDiv)
+        //console.log(picture)
+        let slides = document.querySelectorAll(".imageSlides")
+        let slideIndex = 0;
+        showSlides();
+        function showSlides() {
+            let i;
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";}
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 3000); // Change image every 2 seconds
+    }
+    }) 
+    
+     
+}
+  
 
+
+ 
+
+
+
+searchButton.addEventListener('click',(e) =>{
+    e.preventDefault()
+    
+    initalArray()
 })
+
