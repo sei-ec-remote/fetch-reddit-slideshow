1c5c09b93cc263c636c67a4c88089d3c389b2ff1
CLAUDIO-FLORES1 <CLAUDIOFLRS1@GMAIL.COM>
2022-09-21 16:53:31 -0400

    Completed deliverable

--- index.js
+++ index.js
@@ -5,30 +5,61 @@ const form = document.querySelector('#form');
 
 
 picsArray = [];
+let stopBtn = document.getElementById("stop-picture");
+stopBtn.style.visibility = "hidden";
 
-const makePicsArray = (resArray) => {
-    const arr = resArray.data;
-    const postsArray = arr.children
-    postsArray.forEach(post => {
-        const childData = post.data;
-        const thumbLink = childData.thumbnail
-        if (thumbLink !== "self") {
-            linkArray.push(thumbLink)
-        }
-    })
-    console.log(linkArray)
+let searchImg = document.getElementById("search-img");
+let title = document.getElementById("title");
+let inptBox = document.getElementById("inpt-box");
+let submitBtn = document.getElementById("submit-btn");
+
+const makePicsArray = () => {
+    keyWord = inptBox.value;
+    let url = `http://www.reddit.com/search.json?q=${keyWord}+nsfw:no`;
+    console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => 
+        {
+            for (var i = 0; i < 25; i++)
+            {
+                picsArray.push(data.data.children[i].data.thumbnail);
+            }
+        })
+    stopBtn.style.visibility = "visible";
+    title.style.visibility = "hidden";
+    inptBox.style.visibility = "hidden";
+    submitBtn.style.visibility = "hidden";
+
+    startShow();
 }
 
 
+let idx = 0;
 
-
-const showPicture = (event) => {
-    fetch('https://b.thumbs.redditmedia.com/FlTsywm4ZB29Hd2FulttHossypL3ixa5R0_g7R3TOuQ.jpg')
-        .then(res => res.json())
-        .catch(console.error)
-    const picsUrl = event.target.getAttribute('data-url')
-  
-    fetch(picsUrl)
-        .then(res => res.json())
-        .catch(console.error)
+const updateSlide = () => {
+    if (picsArray[idx] == "self")
+    {
+        idx = idx + 1;
+    }
+    searchImg.src = picsArray[idx];
+    idx = idx + 1;
 }
+
+let timer;
+
+const startShow = () => {
+    timer = setInterval(updateSlide, 1000);
+}
+
+function stopSlide()
+{
+    clearInterval(timer);
+    searchImg.src = "";
+    title.style.visibility = "visible";
+    inptBox.style.visibility = "visible";
+    submitBtn.style.visibility = "visible";
+    stopBtn.style.visibility = "hidden";
+    picsArray = [];
+}
+
